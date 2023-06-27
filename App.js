aafaeb3ec6c0f38e631508a7be1f657f15640192
ally4 <el.ally741@gmail.com>
2023-06-27 13:20:53 +0200

    commenting for debug

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Import your screen components
 import HomeScreen from './app/screens/landing-page/LandingPage';
 import LoginScreen from './app/screens/login/Login';
 
-// Create a stack navigator
 const Stack = createStackNavigator();
 
 function App() {
