4df80c930d5c1f237ed1e0cd099f15dbe4fd6b15
ally4 <el.ally741@gmail.com>
2023-06-27 14:35:24 +0200

    fixing

--- app/screens/login/Login.js
+++ app/screens/login/Login.js
@@ -1,13 +1,6 @@
 import 'react-native-gesture-handler';
-// import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput } from 'react-native';
-// import { NavigationContainer} from '@react-navigation/native';
-// import SignupScreen from '../signup/Signup';
 import { CheckBox } from 'react-native-elements';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// const Stack = createNativeStackNavigator();
 
 
 export default function Login() {
@@ -15,15 +8,8 @@ export default function Login() {
 
   return (
       <SafeAreaView style={styles.container}> 
-      {/* <NavigationContainer> */}
-      {/* <Stack.Navigator> */}
         <View style={{backgroundColor:"black", width:250, height:250, borderRadius:150, opacity:0.2, top: -90, left:-90}}></View>
         <Image source={require("../../assets/photos/colab.png")} style={{marginBottom:20, marginLeft:70}}/>
-        {/* <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SignupScreen')}> */}
-        {/* <Stack.Screen name="Signu[" component={SignupScreen} /> */}
-      {/* <Text style={styles.buttonText}>Login</Text> */}
-    {/* </TouchableOpacity> */}
-        {/* <Button title="Login" backgroundColor="#2FCBD8"  buttonText="#2FCBD8" color="white" width='50' height='50' borderRadius="50" borderColor="white" /> */}
         <View style={{backgroundColor:"white", flex:2, borderTopRightRadius:30, borderTopLeftRadius:30}}>
         <TextInput
           style={{
@@ -66,9 +52,6 @@ export default function Login() {
            <Text style={{marginLeft:90}}>Do you have an account?<TouchableOpacity ><Text style={{ color: '#2FCBD8', marginTop:2}}> Signup</Text></TouchableOpacity></Text>
            <Image source={require("../../assets/photos/acubed.png")} style={{marginBottom:20, marginLeft:150}}/>
         </View>
-        {/* <View style={{backgroundColor:"green", flex:1}}></View> */}
-        {/* </Stack.Navigator> */}
-        {/* </NavigationContainer> */}
       </SafeAreaView>
   );
 }
