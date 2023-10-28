afe4c645e8883ad114dedc5e5dd4df8c951e09c7
ally4 <el.ally741@gmail.com>
2023-10-28 09:58:03 +0200

    the booking

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import store from './app/store';
 import { Provider } from "react-redux";
@@ -14,30 +16,127 @@ import PinScreen from './app/screens/reset-password-pin-screen/ResetPasswordScre
 import UpdateProfil from './app/screens/update-profil/UpdateProfil'
 import Profil from './app/screens/profil-page/ProfilPage'
 import Order from './app/screens/order/Order'
+import Booking from './app/screens/booking/Booking'
 
 const Stack = createNativeStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+// const TabNavigator = () => {
+//   <Tab.Navigator>
+//     <Tab.Screen name='Order' component={Order} />
+//     <Tab.Screen name='Book' component={Order} />
+//   </Tab.Navigator>
+// }
+
 
 const App = () => {
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={LandingPage} options={{ headerShown: false }}           
-        // options={{title: 'Welcome'}}           
-        />
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-         <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
-        <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
-        <Stack.Screen name="ResetEmailScreen" component={ResetEmailScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Pin-Screen" component={PinScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="UpdateProfil" component={UpdateProfil} options={{ headerShown: false }} />
-        <Stack.Screen name="Profil" component={Profil} options={{ headerShown: false }} />
+    <NavigationContainer options={{ headerShown: false }}>
+      <Stack.Navigator mode="modal" options={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={LandingPage} options={{ headerShown: false }}/>
+        <Stack.Screen name="Login" component={Login}  options={{ headerShown: false }}/>
+        <Stack.Screen name="Signup" component={Signup}  options={{ headerShown: false }}/>
         <Stack.Screen name="Order" component={Order} options={{ headerShown: false }} /> 
+        <Stack.Screen name="Booking" component={Booking} options={{ headerShown: false }} /> 
+        <Stack.Screen name="Main" component={Main}  options={{ headerShown: false }}/>
+        {/* <Stack.Screen name="ResetEmailScreen" component={ResetEmailScreen}  /> */}
+        {/* <Stack.Screen name="Pin-Screen" component={PinScreen}  /> */}
+        <Stack.Screen name="UpdateProfil" component={UpdateProfil}  options={{ headerShown: false }}/>
+        {/* <Stack.Screen name="Profil" component={Profil}  /> */}
       </Stack.Navigator>
-    </NavigationContainer>
-    </Provider>
+     </NavigationContainer>
+     </Provider>
   );
 };
+
+
+// const OrderStack = () => {
+//   return (
+//     // <Provider store={store}>
+//     // <NavigationContainer>
+//       <Stack.Navigator mode="modal" options={{ headerShown: false }}>
+//         {/* <Stack.Screen name="Home" component={LandingPage} /> */}
+//         {/* <Stack.Screen name="Login" component={Login}  /> */}
+//         {/* <Stack.Screen name="Signup" component={Signup}  /> */}
+//         <Stack.Screen name="Order" component={Order} options={{ headerShown: false }} /> 
+//         <Stack.Screen name="Main" component={Main}  options={{ headerShown: false }}/>
+//         {/* <Stack.Screen name="ResetEmailScreen" component={ResetEmailScreen}  /> */}
+//         {/* <Stack.Screen name="Pin-Screen" component={PinScreen}  /> */}
+//         <Stack.Screen name="UpdateProfil" component={UpdateProfil}  />
+//         {/* <Stack.Screen name="Profil" component={Profil}  /> */}
+//       </Stack.Navigator>
+//     // </NavigationContainer>
+//     // </Provider>
+//   );
+// };
+
+
+// const BookingStack = () => {
+//   return (
+//     // <Provider store={store}>
+//     // <NavigationContainer>
+//       <Stack.Navigator mode="modal" options={{ headerShown: false }}>
+//         {/* <Stack.Screen name="Home" component={LandingPage} /> */}
+//         {/* <Stack.Screen name="Login" component={Login}  /> */}
+//         {/* <Stack.Screen name="Signup" component={Signup}  /> */}
+//         <Stack.Screen name="Main" component={Main}  options={{ headerShown: false }}/>
+//         {/* <Stack.Screen name="ResetEmailScreen" component={ResetEmailScreen}  /> */}
+//         {/* <Stack.Screen name="Pin-Screen" component={PinScreen}  /> */}
+//         {/* <Stack.Screen name="UpdateProfil" component={UpdateProfil}  /> */}
+//         <Stack.Screen name="Order" component={Order} options={{ headerShown: false }} /> 
+//         {/* <Stack.Screen name="Profil" component={Profil}  /> */}
+//       </Stack.Navigator>
+//     // </NavigationContainer>
+//     // </Provider>
+//   );
+// };
+
+
+
+
+// const TabNavigator = () => {
+//   return (
+//       <Tab.Navigator options={{ headerShown: false }}>
+//       {/* <Tab.Screen name="MainStack" component={MainStack} options={{ headerShown: false }}/> */}
+//         <Tab.Screen name="Home" component={LandingPage} />
+//         <Tab.Screen name="Login" component={Login}  />
+//         <Stack.Screen name="Signup" component={Signup}  />
+//         <Tab.Screen name="OrderStack" component={OrderStack}  options={{ headerShown: false, tabBarIcon:()=><MaterialCommunityIcons name="test-tube" color="#2FCBD8" size={25}/> }}/>
+//         {/* <Stack.Screen name="ResetEmailScreen" component={ResetEmailScreen}  /> */}
+//         {/* <Stack.Screen name="Pin-Screen" component={PinScreen}  /> */}
+//         {/* <Stack.Screen name="UpdateProfil" component={UpdateProfil}  /> */}
+//         {/* <Stack.Screen name="Profil" component={Profil}  /> */}
+//         <Tab.Screen name="BookingStack" component={BookingStack} options={{ headerShown: false }} /> 
+//       </Tab.Navigator>
+//   );
+// };
+
+
+
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//     <NavigationContainer>
+//     <TabNavigator />
+//          {/* <Stack.Navigator mode="modal" options={{ headerShown: false }}> */}
+//          {/* <Stack.Screen name="Home" component={LandingPage} /> */}
+//          {/* <Stack.Screen name="Login" component={Login}  /> */}
+//          {/* <Stack.Screen name="Signup" component={Signup}  /> */}
+//          {/* <Stack.Screen name="Main" component={Main}  options={{ headerShown: false }}/> */}
+//          {/* <Stack.Screen name="Booking" component={Booking}  options={{ headerShown: false }}/> */}
+//          {/* <Stack.Screen name="ResetEmailScreen" component={ResetEmailScreen}  /> */}
+//          {/* <Stack.Screen name="Pin-Screen" component={PinScreen}  /> */}
+//          {/* <Stack.Screen name="UpdateProfil" component={UpdateProfil}  /> */}
+//          {/* <Stack.Screen name="Order" component={Order} options={{ headerShown: false }} />  */}
+//          {/* <Stack.Screen name="Profil" component={Profil}  /> */}
+//        {/* </Stack.Navigator> */}
+//     </NavigationContainer>
+//     </Provider>
+//   );
+// };
+
 
 export default App;
 
